4e38efc61309b44800279293f50d3a0a32c073b9
Bui The Anh <bupbui95@gmail.com>
2021-07-20 21:44:04 +0700

    remove .env

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 // PORT : 
 const PORT = process.env.PORT || 4000;
 
-
+//https://account.mongodb.com/account/login?signedOut=true
 
 app.listen(PORT,() => {
     console.log(`server is running in port : ${PORT}`);
