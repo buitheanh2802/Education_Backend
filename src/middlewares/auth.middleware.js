5e4b22c075ff85cc118aaf54f481d06c50f2f020
Bui The Anh <bupbui95@gmail.com>
2021-08-01 21:09:46 +0700

    update

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,1 +1,8 @@
 // các middle liên quan đến phân quyền và token
+import expressRateLimit from 'express-rate-limit';
+
+export const apiLimiter = expressRateLimit({
+    max : 1,
+    windowMs : 1000 * 20,
+    message : 'Đẩy cc gì nhiều thế'
+})
--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { create, fetchAll, read, remove, update } from '../controllers/comment.controller';
 import { commentById, commentValidator } from '../middlewares/comment.middleware';
+import { apiLimiter } from '../middlewares/auth.middleware';
 
 const router = express.Router();
 
 router.get('/:commentId', read)
 router.get('/', fetchAll)
-router.post('/', commentValidator, create)
+router.post('/',apiLimiter,commentValidator, create)
 router.put('/:commentId', commentValidator, update)
 router.delete('/:commentId', remove)
 
