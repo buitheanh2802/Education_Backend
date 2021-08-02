6ec01397304f1d1b55d9402bece27a97a40ee494
Bui The Anh <bupbui95@gmail.com>
2021-08-02 21:22:53 +0700

    done sendMail

--- src/helpers/useMailer.js
+++ src/helpers/useMailer.js
@@ -1,11 +1,13 @@
 import nodemailer from 'nodemailer';
-
+// use handlebars to write template send mail
+import handlebars from 'nodemailer-express-handlebars';
+import path from 'path';
 // gmail for team
 // gmail : devchallenge113@gmail.com
 // password : devchallenge@2021
 
 // config mail server
-const createTransport = nodemailer.createTransport({
+const transportion = nodemailer.createTransport({
     port : 587, //port default for protocol SMTP
     host : 'smtp.gmail.com',
     secure : true,
@@ -15,4 +17,19 @@ const createTransport = nodemailer.createTransport({
     }
 });
 
-export const sendMail = async() => await createTransport.sendMail();
+// initial middleware of transport
+transportion.use('compile',handlebars({
+    extName : '.hbs',
+    viewPath : path.resolve(__dirname,'../assets/template'),
+    viewEngine : {
+        extname : '.hbs'
+    }
+}))
+
+export const sendMail = async(sendTo,subject,template) => await transportion.sendMail({
+    from : `Devchallenge VerifyAccount <devchallenge123@gmail.com>`,
+    to : sendTo,
+    replyTo : 'devchallenge123@gmail.com',
+    subject,
+    template
+});
