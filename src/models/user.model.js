ca1f4fcaa1efba819270a1176d3e0ca03a337d4a
Bui The Anh <bupbui95@gmail.com>
2021-08-07 01:25:29 +0700

    update user.model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,2 +1,26 @@
 import mongoose from 'mongoose';
 
+const initializeSchema = mongoose.Schema;
+
+const UserSchema = new initializeSchema({
+    fullName : { type : String },
+    username : { type : String },
+    email : { type : String },
+    password : { type : String },
+    avatar : {
+        id : String,
+        linkUrl : String
+    },
+    address : String,
+    phoneNumber : String,
+    role : String,
+    type : String,
+    secretkey : String
+},
+{
+    timestamps : true
+});
+
+UserSchema.methods = {
+    // verifyPassword : 
+}
