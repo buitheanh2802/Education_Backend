4d2bee3f250bff60f6e3c679c300756e8574509d
Bui The Anh <bupbui95@gmail.com>
2021-09-08 20:35:29 +0700

    update

--- src/services/system.js
+++ src/services/system.js
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import path from 'path';
-import { path as systemPath } from 'constants/routeDefination';
 
 // remove file system
 export const removeFileSystem = (filename) => {
