1a1bdbb09d1b8524b17ee62b000f70253f3eb936
haiden97tn <haiden97tn@gmail.com>
2021-10-12 22:56:29 +0700

    up code get list & delete image

--- src/controllers/picture.controller.js
+++ src/controllers/picture.controller.js
@@ -2,10 +2,9 @@ import { response } from "constants/responseHandler";
 import formidable from "formidable";
 import { cropper } from "helpers/imageCropper";
 import PictureModel from "models/picture.model";
-import { createFile } from "services/drive";
+import { createFile, deleteFile } from "services/drive";
 
 export const create = (req, res) => {
-
     const initialize = new formidable.IncomingForm({
         maxFileSize: 1024 * 1024,
         keepExtensions: true
@@ -35,5 +34,28 @@ export const create = (req, res) => {
             return response(res, 200, [], docs)
         })
 
+    })
+}
+export const gets = (req, res) => {
+    PictureModel.find({ createBy: req.userId }, (err, docs) => {
+        if (err) return response(res, 500, ['ERROR_SERVER']);
+        return response(res, 200, [], docs);
+    })
+}
+export const remove = (req, res) => {
+    //xoa trong google drive
+    PictureModel.find({ _id: req.params.pictureId }, (err, docs) => {
+        if (err) return response(res, 500, ['ERROR_SERVER', err.message]);
+        if (docs.length == 0) {
+            return response(res, 400, ['Can not find id!']);
+        }
+        const fileId = docs[0].photo.id;
+        deleteFile(fileId)
+    })
+    //xoa trong db
+    PictureModel.deleteOne({ _id: req.params.pictureId }, (err, docs) => {
+        if (err) return response(res, 500, ['ERROR_SERVER', err.message]);
+        if (!docs) return response(res, 400, ['EMPTY_DATA']);
+        return response(res, 200, []);
     })
 }
--- src/routes/picture.route.js
+++ src/routes/picture.route.js
@@ -1,10 +1,12 @@
 import { path } from 'constants/routeDefination';
-import { create } from 'controllers/picture.controller';
+import { create, gets, remove } from 'controllers/picture.controller';
 import express from 'express';
 import { accessToken } from 'middlewares/auth.middleware';
 
 const router = express.Router();
 
 router.post(path.picture.post, accessToken, create);
+router.get(path.picture.gets, accessToken, gets);
+router.delete(path.picture.delete, accessToken, remove);
 
 export default router;
